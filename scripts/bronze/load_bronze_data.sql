8d98b925a50fef4d45f2316d8e0cb3b7424711e8
VivekPanthii <161216319+VivekPanthii@users.noreply.github.com>
2025-05-15 14:52:39 +0545

    Update load_bronze_data.sql

--- scripts/bronze/load_bronze_data.sql
+++ scripts/bronze/load_bronze_data.sql
@@ -11,41 +11,68 @@ SET GLOBAL local_infile = 1;
 SET GLOBAL local_infile = 1;
 SHOW VARIABLES LIKE 'local_infile';
 
--- truncate all bronze tables and reload csv data
-    TRUNCATE TABLE bronze.crm_cust_info;
-    LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_crm/cust_info.csv'
-    INTO TABLE bronze.crm_cust_info
-    FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+-- ============================================
+--            Loading Bronze Layer
+-- ============================================
 
+-- Truncate all bronze tables and reload CSV data
 
-    TRUNCATE TABLE bronze.crm_prd_info;
-    LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_crm/prd_info.csv'
-    INTO TABLE bronze.crm_prd_info
-    FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+-- CRM Tables
+SELECT '>> Truncating Table: bronze.crm_cust_info';
+TRUNCATE TABLE bronze.crm_cust_info;
 
-    TRUNCATE TABLE bronze.crm_sales_info;
-    LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_crm/sales_details.csv'
-    INTO TABLE bronze.crm_sales_info
-    FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+SELECT '>> Inserting Data Into: bronze.crm_cust_info';
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_crm/cust_info.csv'
+INTO TABLE bronze.crm_cust_info
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES;
 
-    TRUNCATE TABLE bronze.erp_cust_az12;
-    LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_erp/cust_az12.csv'
-    INTO TABLE bronze.erp_cust_az12
-    FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+SELECT '>> Truncating Table: bronze.crm_prd_info';
+TRUNCATE TABLE bronze.crm_prd_info;
 
+SELECT '>> Inserting Data Into: bronze.crm_prd_info';
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_crm/prd_info.csv'
+INTO TABLE bronze.crm_prd_info
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES;
 
-    TRUNCATE TABLE bronze.erp_loc_a101;
-    LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_erp/loc_a101.csv'
-    INTO TABLE bronze.erp_loc_a101
-    FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+SELECT '>> Truncating Table: bronze.crm_sales_info';
+TRUNCATE TABLE bronze.crm_sales_info;
 
-    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-    LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_erp/px_cat_g1v2.csv'
-    INTO TABLE bronze.erp_px_cat_g1v2
-    FIELDS TERMINATED BY ','
-    IGNORE 1 LINES;
+SELECT '>> Inserting Data Into: bronze.crm_sales_info';
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_crm/sales_details.csv'
+INTO TABLE bronze.crm_sales_info
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES;
+
+-- ============================================
+--            Loading Bronze Layer
+-- ============================================
+
+-- ERP Tables
+SELECT '>> Truncating Table: bronze.erp_cust_az12';
+TRUNCATE TABLE bronze.erp_cust_az12;
+
+SELECT '>> Inserting Data Into: bronze.erp_cust_az12';
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_erp/cust_az12.csv'
+INTO TABLE bronze.erp_cust_az12
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES;
+
+SELECT '>> Truncating Table: bronze.erp_loc_a101';
+TRUNCATE TABLE bronze.erp_loc_a101;
+
+SELECT '>> Inserting Data Into: bronze.erp_loc_a101';
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_erp/loc_a101.csv'
+INTO TABLE bronze.erp_loc_a101
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES;
+
+SELECT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+SELECT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+LOAD DATA LOCAL INFILE 'C:/Program Files/MySQL/MySQL Server 8.0/source_erp/px_cat_g1v2.csv'
+INTO TABLE bronze.erp_px_cat_g1v2
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES;
